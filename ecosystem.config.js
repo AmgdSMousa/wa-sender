57a0005dba3e6f3e8c1eda74babf4688e13ab84b
amged <amged@Amged.Amged>
2026-07-26 12:48:12 +0300

    Set ecosystem.config.js to dev mode for instant VPS launch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
     {
       name: 'wa-sender',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3000',
+      args: 'dev -p 3000',
       exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: 'development',
         PORT: 3000
       }
     }
