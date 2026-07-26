b6047476908a5527b538f41036f121d19254a486
amged <amged@Amged.Amged>
2026-07-26 13:32:03 +0300

    Switch PM2 to production mode for instant loading speed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
     {
       name: 'wa-sender',
       script: 'node_modules/next/dist/bin/next',
-      args: 'dev -p 3000',
+      args: 'start -p 3000',
       exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
-        NODE_ENV: 'development',
+        NODE_ENV: 'production',
         PORT: 3000
       }
     }
